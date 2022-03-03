f667ce236b1675b111c1c46a83659c78373d5929
Idris Bulut <idrisbulut00@gmail.com>
2022-03-03 23:52:45 +0300

    logging

--- app/src/main/java/com/dreyzz/jettrivia/MainActivity.kt
+++ app/src/main/java/com/dreyzz/jettrivia/MainActivity.kt
@@ -1,15 +1,19 @@
 package com.dreyzz.jettrivia
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
-import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.hilt.navigation.compose.hiltViewModel
+import com.dreyzz.jettrivia.ui.screens.QuestionsViewModel
 import com.dreyzz.jettrivia.ui.theme.JetTriviaTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -17,7 +21,7 @@ class MainActivity : ComponentActivity() {
             JetTriviaTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
+                    TriviaHome()
                 }
             }
         }
@@ -25,14 +29,31 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun TriviaHome(viewModel: QuestionsViewModel = hiltViewModel()) {
+    Questions(viewModel)
+}
+
+@Composable
+fun Questions(viewModel: QuestionsViewModel) {
+    val questions = viewModel.data.value.data?.toMutableList()
+
+    if (viewModel.data.value.loading == true)
+        Log.d("Loading", "Questions: ...Loading...")
+    else {
+        Log.d("Loading", "Questions: Loading STOPPED...")
+
+        questions?.forEach {
+            Log.d("Result", "Questions: ${it.question}")
+        }
+    }
+
+    Log.d("SIZE", "Questions: ${questions?.size}")
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     JetTriviaTheme {
-        Greeting("Android")
+
     }
 }
