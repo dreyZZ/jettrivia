a80456f04234411972748309e7456ad610e3795a
Idris Bulut <idrisbulut00@gmail.com>
2022-03-04 23:53:42 +0300

    refactored - create new composable

--- app/src/main/java/com/dreyzz/jettrivia/ui/component/QuestionOrProgressView.kt
+++ app/src/main/java/com/dreyzz/jettrivia/ui/component/QuestionOrProgressView.kt
@@ -113,63 +113,7 @@ fun QuestionView(
                     lineHeight = 22.sp,
                     color = AppColors.mOffWhite
                 )
-
-                // choices
-                choicesState.forEachIndexed { choiceIndex, answerText ->
-                    Row(
-                        modifier = Modifier
-                            .padding(3.dp)
-                            .fillMaxWidth()
-                            .height(45.dp)
-                            .border(
-                                width = 4.dp,
-                                brush = Brush.linearGradient(
-                                    colors = listOf(
-                                        AppColors.mOffDarkPurple,
-                                        AppColors.mOffDarkPurple
-                                    )
-                                ),
-                                shape = RoundedCornerShape(15.dp)
-                            )
-                            .clip(RoundedCornerShape(percent = 50))
-                            .background(Color.Transparent),
-                        verticalAlignment = Alignment.CenterVertically
-                    ) {
-                        RadioButton(
-                            selected = answerState.value == choiceIndex,
-                            onClick = {
-                                updateAnswer(choiceIndex)
-                            },
-                            modifier = Modifier.padding(start = 16.dp),
-                            colors = RadioButtonDefaults.colors(
-                                selectedColor = if (correctAnswerState.value == true && choiceIndex == answerState.value) {
-                                    Color.Green.copy(alpha = 0.2f)
-                                } else {
-                                    Color.Red.copy(alpha = 0.2f)
-                                }
-                            )
-                        )
-                        val annotatedString = buildAnnotatedString {
-                            withStyle(
-                                style = SpanStyle(
-                                    fontWeight = FontWeight.Light,
-                                    color = if (correctAnswerState.value == true && choiceIndex == answerState.value) {
-                                        Color.Green
-                                    } else if (correctAnswerState.value == false && choiceIndex == answerState.value) {
-                                        Color.Red
-                                    } else {
-                                        AppColors.mOffWhite
-                                    },
-                                    fontSize = 17.sp
-                                )
-                            ) {
-                                append(answerText)
-                            }
-                        }
-                        Text(annotatedString, modifier = Modifier.padding(6.dp))
-                    }
-                }
-
+                ChoicesView(choicesState, answerState, updateAnswer, correctAnswerState)
                 Button(
                     onClick = { onNextClicked(questionIndexState.value) },
                     modifier = Modifier
@@ -189,6 +133,69 @@ fun QuestionView(
             }
         }
 
+    }
+}
+
+@Composable
+private fun ChoicesView(
+    choicesState: MutableList<String>,
+    answerState: MutableState<Int?>,
+    updateAnswer: (Int) -> Unit,
+    correctAnswerState: MutableState<Boolean?>
+) {
+    choicesState.forEachIndexed { choiceIndex, answerText ->
+        Row(
+            modifier = Modifier
+                .padding(3.dp)
+                .fillMaxWidth()
+                .height(45.dp)
+                .border(
+                    width = 4.dp,
+                    brush = Brush.linearGradient(
+                        colors = listOf(
+                            AppColors.mOffDarkPurple,
+                            AppColors.mOffDarkPurple
+                        )
+                    ),
+                    shape = RoundedCornerShape(15.dp)
+                )
+                .clip(RoundedCornerShape(percent = 50))
+                .background(Color.Transparent),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            RadioButton(
+                selected = answerState.value == choiceIndex,
+                onClick = {
+                    updateAnswer(choiceIndex)
+                },
+                modifier = Modifier.padding(start = 16.dp),
+                colors = RadioButtonDefaults.colors(
+                    selectedColor = if (correctAnswerState.value == true && choiceIndex == answerState.value) {
+                        Color.Green.copy(alpha = 0.2f)
+                    } else {
+                        Color.Red.copy(alpha = 0.2f)
+                    }
+                )
+            )
+            val annotatedString = buildAnnotatedString {
+                withStyle(
+                    style = SpanStyle(
+                        fontWeight = FontWeight.Light,
+                        color = if (correctAnswerState.value == true && choiceIndex == answerState.value) {
+                            Color.Green
+                        } else if (correctAnswerState.value == false && choiceIndex == answerState.value) {
+                            Color.Red
+                        } else {
+                            AppColors.mOffWhite
+                        },
+                        fontSize = 17.sp
+                    )
+                ) {
+                    append(answerText)
+                }
+            }
+            Text(annotatedString, modifier = Modifier.padding(6.dp))
+        }
     }
 }
 
